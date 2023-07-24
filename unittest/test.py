52cb3dbf569a6080e4bb2133a1750df424443cbe
SiraYsia <ytamiru@terpmail.umd.edu>
2023-07-24 18:03:54 -0400

    requirments.txt

--- unittest/test.py
+++ unittest/test.py
@@ -15,9 +15,46 @@ class BasicTests(unittest.TestCase):
     #### tests ####
     ###############
 
-    def test_main_page(self):
+    # def test_main_page(self):
+    #     response = self.app.get('/', follow_redirects=True)
+    #     self.assertEqual(response.status_code, 200)
+    
+    def test_index(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-
+    
+    def test_login(self):
+        response = self.app.get('/login', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_signup(self):
+        response = self.app.get('/signup', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_started(self):
+        response = self.app.get('/getstarted', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_create_flashcards(self):
+        response = self.app.get('/createflashcards', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    
+    def test_your_flashcards(self):
+        response = self.app.get('/your-flashcards', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_success(self):
+        response = self.app.get('/success', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    # def test_single_flashcard(self):
+    #     response = self.app.get('/single-flashcard', follow_redirects=True)
+    #     self.assertEqual(response.status_code, 200)
+    
+    def test_single_about_us(self):
+        response = self.app.get('/aboutus', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
 if __name__ == "__main__":
     unittest.main()
