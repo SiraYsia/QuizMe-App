1be3eb9993a7b7f8749783a4c782da70aa358e90
Yeabsira Tamiru <ytamiru@terpmail.umd.edu>
2024-09-14 01:29:20 -0400

    add uri

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quizme.db'
 
 
 db = SQLAlchemy(app)
